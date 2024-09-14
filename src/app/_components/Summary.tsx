4013da998919bf18fe4126684f23e2de543b8162
Anuj <anujjoshi63@gmail.com>
2024-09-14 13:54:06 -0700

    separate out the summary component

---
+++ src/app/_components/Summary.tsx
@@ -0,0 +1,50 @@
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+import clsx from "clsx";
+
+const Summary = async () => {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const userHabits = await api.habit.getHabits();
+  const { points } = await api.post.getPoints();
+  const earnedActions = Object.values(userHabits)
+    .filter((el) => el.points <= 0 && Math.abs(el.points) <= points)
+    .reduce((acc, el) => `${acc} ${el.name};;`, "")
+    .split(";;")
+    .filter((el) => el);
+  return (
+    <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
+      {points ? (
+        <p
+          className={clsx("-mt-3 text-2xl font-semibold", {
+            "text-rose-400": points < 0,
+            "text-emerald-500": points > 0,
+            "text-gray-400": points === 0,
+          })}
+        >
+          {points} points
+        </p>
+      ) : (
+        <p>You have no points yet.</p>
+      )}
+      {earnedActions.length > 0 ? (
+        <ul className="text-xl font-semibold">
+          You can{" "}
+          {earnedActions.map(
+            (el, i) =>
+              el.length > 0 && (
+                <li key={el + i} className="text-lg font-normal">
+                  -{el}
+                </li>
+              ),
+          )}
+        </ul>
+      ) : (
+        <p>You haven&apos;t worked enough to have fun 🤨</p>
+      )}
+    </div>
+  );
+};
+
+export default Summary;
--- src/app/crud-showcase/page.tsx
+++ src/app/crud-showcase/page.tsx
@@ -1,8 +1,8 @@
 import { CreatePost } from "@/app/_components/create-post";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
-import clsx from "clsx";
 import Link from "next/link";
+import Summary from "../_components/Summary";
 export default async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
@@ -10,44 +10,10 @@ export default async function CrudShowcase() {
   await api.habit.checkTemplateHabits();
   const latestPosts = await api.post.getLatest();
   const userHabits = await api.habit.getHabits();
-  const { points } = await api.post.getPoints();
-  const earnedActions = Object.values(userHabits)
-    .filter((el) => el.points <= 0 && Math.abs(el.points) <= points)
-    .reduce((acc, el) => `${acc} ${el.name};;`, "")
-    .split(";;")
-    .filter((el) => el);
+
   return (
     <div className="flex w-full max-w-2xl flex-col gap-8">
-      <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
-        {points ? (
-          <p
-            className={clsx("-mt-3 text-2xl font-semibold", {
-              "text-rose-500": points < 0,
-              "text-emerald-500": points > 0,
-              "text-gray-400": points === 0,
-            })}
-          >
-            {points} points
-          </p>
-        ) : (
-          <p>You have no points yet.</p>
-        )}
-        {earnedActions.length > 0 ? (
-          <ul className="text-xl font-semibold">
-            You can{" "}
-            {earnedActions.map(
-              (el, i) =>
-                el.length > 0 && (
-                  <li key={el + i} className="text-lg font-normal">
-                    -{el}
-                  </li>
-                ),
-            )}
-          </ul>
-        ) : (
-          <p>You haven&apos;t worked enough to have fun 🤨</p>
-        )}
-      </div>
+      <Summary />
       {latestPosts && latestPosts.length > 0 ? (
         <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
           <div className="flex items-center justify-between truncate text-2xl font-semibold">
