b41da76f41de0e968307aecc92e6a020bc5c1fef
Anuj <anujjoshi63@gmail.com>
2024-09-15 18:22:58 -0700

    make latestPosts a client component to show loading state and faster page load

--- src/app/_components/latest-posts.tsx
+++ src/app/_components/latest-posts.tsx
@@ -1,7 +1,11 @@
-import { api } from "@/trpc/server";
+"use client";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/trpc/react"; // Adjust import to client-side usage
+import clsx from "clsx";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-async function LatestPosts({
+function LatestPosts({
   userHabits,
 }: {
   userHabits: Record<
@@ -14,43 +18,78 @@ async function LatestPosts({
     }
   >;
 }) {
-  const latestPosts = await api.post.getLatest();
-  if (latestPosts && latestPosts.length > 0) {
-    return (
-      <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
-        <div className="flex items-center justify-between truncate text-2xl font-semibold">
-          Logs
-          <Link
-            href="/logs"
-            className="rounded-full bg-white bg-opacity-10 px-3 py-1 text-sm opacity-75 transition-all hover:bg-opacity-20 hover:opacity-90"
-          >
-            see all
-          </Link>
-        </div>
-        <div className="flex flex-col gap-2">
-          {latestPosts.map((post) => {
-            const habit = userHabits?.[post.habitId];
-            if (!habit) return null;
-            const pointsDisplay =
-              habit?.points > 0 ? `+${habit.points}` : habit?.points;
+  const { data: latestPosts, isLoading } = api.post.getLatest.useQuery();
+  const [hasLoaded, setHasLoaded] = useState(false);
+  useEffect(() => {
+    if (!isLoading) {
+      const timeout = setTimeout(() => {
+        setHasLoaded(true);
+      }, 300); // Adjust the timeout to match the transition duration
 
-            return (
+      return () => clearTimeout(timeout); // Clean up timeout on component unmount
+    } else {
+      setHasLoaded(false); // Reset state if it goes back to loading
+    }
+  }, [isLoading]);
+
+  return (
+    <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
+      <div className="flex items-center justify-between truncate text-2xl font-semibold">
+        Logs
+        <Link
+          href="/logs"
+          className="rounded-full bg-white bg-opacity-10 px-3 py-1 text-sm opacity-75 transition-all hover:bg-opacity-20 hover:opacity-90"
+        >
+          see all
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        {isLoading
+          ? Array.from({ length: 3 }).map((_, index) => (
               <div
-                className="w-full rounded-xl bg-white bg-opacity-5 px-4 py-3"
-                key={post.id}
+                key={index}
+                className="flex h-16 w-full animate-pulse flex-col justify-evenly gap-2 rounded-xl bg-white bg-opacity-5 p-4"
               >
                 <div className="flex justify-between">
-                  <div>{habit?.name}</div>
-                  <div>{pointsDisplay}</div>
+                  <Skeleton className="h-3 w-[15ch] rounded-full opacity-50" />
+                  <Skeleton className="h-3 w-[6ch] rounded-full opacity-50" />
                 </div>
-                <div>{new Date(post.createdAt).toDateString()}</div>
+                <div>
+                  <Skeleton className="h-3 w-[10ch] rounded-full opacity-50" />
+                </div>
               </div>
-            );
-          })}
-        </div>
+            ))
+          : latestPosts?.map((post) => {
+              const habit = userHabits?.[post.habitId];
+              if (!habit) return null;
+              const pointsDisplay =
+                habit?.points > 0 ? `+${habit.points}` : habit?.points;
+
+              return (
+                <div
+                  className={clsx(
+                    "h-16 w-full rounded-xl bg-white bg-opacity-5 px-4 py-3 transition-opacity duration-300",
+                    {
+                      "opacity-100": hasLoaded,
+                      "opacity-5": !hasLoaded,
+                    },
+                  )}
+                  style={{
+                    transitionTimingFunction: "cubic-bezier(.21,.58,.69,.66)",
+                  }}
+                  key={post.id}
+                >
+                  <div className="flex justify-between">
+                    <div>{habit?.name}</div>
+                    <div>{pointsDisplay}</div>
+                  </div>
+                  <div>{new Date(post.createdAt).toDateString()}</div>
+                </div>
+              );
+            })}
       </div>
-    );
-  } else return <p>You have no posts yet.</p>;
+    </div>
+  );
 }
 
 export default LatestPosts;
