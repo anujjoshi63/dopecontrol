7de51c4d726c61813fa03be0c950c0eaae8e2d7b
Anuj <anujjoshi63@gmail.com>
2025-01-24 23:26:34 -0700

    fix: update text styling and message in Summary component for clarity and emphasis

--- src/app/_components/Summary.tsx
+++ src/app/_components/Summary.tsx
@@ -27,7 +27,7 @@ const Summary = async ({
     <div className="flex flex-col justify-center gap-4 rounded-xl bg-white bg-opacity-5 p-6">
       {points ? (
         <p
-          className={clsx("-mt-3 text-2xl font-semibold", {
+          className={clsx("-mt-3 text-3xl font-medium", {
             "text-rose-400": points < 0,
             "text-emerald-500": points > 0,
             "text-gray-400": points === 0,
@@ -51,7 +51,11 @@ const Summary = async ({
           )}
         </ul>
       ) : (
-        <p>You haven&apos;t worked enough to have fun 🤨</p>
+        <p className="text-white/70">
+          You haven't worked enough to have fun yet.
+          <br />
+          Start by working on some productive or self-improvement habits.
+        </p>
       )}
     </div>
   );
