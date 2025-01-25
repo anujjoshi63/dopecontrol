9627d4c5884e5cbc3b8a19a05b637c82fe042384
Anuj <anujjoshi63@gmail.com>
2025-01-24 23:25:42 -0700

    fix: adjust layout and typography for improved readability on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default async function Home() {
   const session = await getServerAuthSession();
   if (!session?.user)
     return (
-      <main className="flex min-h-svh flex-col items-center justify-center gap-4">
-        <h1 className="flex flex-col justify-center text-5xl font-extrabold tracking-tight sm:text-[2rem]">
+      <main className="flex min-h-svh flex-col items-center justify-center gap-2">
+        <h1 className="flex flex-col justify-center text-5xl font-extrabold tracking-tighter sm:text-[3rem]">
           <div>
             Dope <span className="text-[hsl(162,78%,42%)]">Control</span>
           </div>
         </h1>
-        <div className="text-base font-semibold tracking-tight">
+        <div className="text-xl font-semibold tracking-tight">
           Don&apos;t just have fun, earn it.
         </div>
         <SignInWithGoogleButton />
@@ -26,9 +26,9 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-1 flex-col items-center justify-start gap-12 px-4 py-6">
-        <h1 className="flex w-full justify-between text-5xl font-extrabold tracking-tight sm:text-[2rem]">
+        <h1 className="flex w-full justify-between text-5xl font-extrabold tracking-tighter sm:text-[2rem]">
           <div></div>
-          <div className="text-center text-3xl lg:text-5xl">
+          <div className="text-center text-3xl lg:text-4xl">
             Dope <span className="text-[hsl(162,78%,42%)]">Control</span>
           </div>
           <ProfilePicture
