a78816cf77b9908e15de038e04e37a8d90c54f17
Anuj <anujjoshi63@gmail.com>
2025-06-26 22:55:50 -0700

    feat: enhance Home component layout with navigation and improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { getServerAuthSession } from "@/server/auth";
 import ProfilePicture from "./_components/ProfilePicture";
 import CrudShowcase from "./_components/crud-showcase";
 import SignInWithGoogleButton from "./_components/login";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { History } from "lucide-react";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -23,15 +26,23 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-1 flex-col items-center justify-start gap-12 px-4 py-6">
-        <h1 className="flex w-full justify-between text-5xl font-extrabold tracking-tighter sm:text-[2rem]">
+        <h1 className="flex w-full justify-between items-center text-5xl font-extrabold tracking-tight sm:text-[2rem]">
           <div className="hidden md:block"></div>
           <div className="text-3xl lg:text-4xl">
             Dope <span className="text-[hsl(162,78%,42%)]">Control</span>
           </div>
-          <ProfilePicture
-            imageURL={session.user.image!}
-            name={session.user.name!}
-          />
+          <div className="flex items-center gap-4 justify-center ">
+            <Link href="/logs" className="flex justify-center items-center">
+              <Button variant="ghost" className="text-white">
+                <History className="w-4 mr-2" />
+                Logs
+              </Button>
+            </Link>
+            <ProfilePicture
+              imageURL={session.user.image!}
+              name={session.user.name!}
+            />
+          </div>
         </h1>
         <CrudShowcase />
       </div>
