b5e593d32c5ba26aa5381a362161e4b7b1531317
Anuj <anujjoshi63@gmail.com>
2025-06-28 22:00:29 -0700

    fix(header): center title on desktop screens
    - Use a responsive grid layout to center the header title on medium screens and larger.
    - The mobile layout remains unchanged.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,12 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-1 flex-col items-center justify-start gap-12 px-4 py-6">
-        <h1 className="flex w-full justify-between items-center text-5xl font-extrabold tracking-tight sm:text-[2rem]">
+        <h1 className="flex w-full items-center justify-between text-5xl font-extrabold tracking-tight sm:text-[2rem] md:grid md:grid-cols-3">
           <div className="hidden md:block"></div>
-          <div className="text-3xl lg:text-4xl">
+          <div className="text-3xl lg:text-4xl md:text-center">
             Dope <span className="text-[hsl(162,78%,42%)]">Control</span>
           </div>
-          <div className="flex items-center gap-4 justify-center ">
+          <div className="flex items-center gap-4 justify-self-end">
             <Link href="/logs" className="flex justify-center items-center">
               <Button variant="ghost" className="text-white">
                 <History className="w-4 mr-2" />
